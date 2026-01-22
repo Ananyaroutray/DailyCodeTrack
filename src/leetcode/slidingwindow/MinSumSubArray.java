778e2b6379afbb3eba06c987d0caf2c928c8e68b
ananyaroutray <ananyaroutray20@gmail.com>
2026-01-22 21:41:00 +0530

    day-22

--- src/leetcode/slidingwindow/MinSumSubArray.java
+++ src/leetcode/slidingwindow/MinSumSubArray.java
@@ -1,27 +1,25 @@
 package leetcode.slidingwindow;
 
-public class MaxSumSubarray {
-
+public class MinSumSubArray {
     public static void main(String[] args) {
         int[] arr = {1,3,2,5,6,0};
-        System.out.println(maxSum(arr,3));
+        System.out.println(minSum(arr,3));
     }
 
-    public static int maxSum(int[] arr, int k){
+    public static int minSum(int[] arr, int k){
 
-        int windowSum =0;
-        int maxSum =0;
+        int windowSum = 0;
+        int minSum = Integer.MAX_VALUE;
+
         for(int i=0;i<arr.length;i++){
+
             windowSum = windowSum + arr[i];
 
-            if(i>=k-1){
-                maxSum = Math.max(maxSum,windowSum);
+            if(i>= k-1){
+                minSum = Math.min(minSum,windowSum);
                 windowSum = windowSum - arr[i-(k-1)];
             }
         }
-
-        return maxSum;
+        return minSum;
     }
-
-
 }
